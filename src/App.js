e4e94977e839f08aeb8b7e8bb3e7e66179ff31f7
hamdain <dain9812@naver.com>
2022-02-21 14:04:56 +0900

    Add useClick hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import useClick from "./hooks/useClick";
 
-function App() {
+const App = () => {
+  const sayHello = () => console.log("say hello");
+  const title = useClick(sayHello);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 ref={title}>Start</h1>
     </div>
   );
-}
+};
 
 export default App;
